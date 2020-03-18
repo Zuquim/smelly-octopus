7a5ae8227f733ca381d6dc89703f12d3d05564d8
Marco Túlio Zuquim <mtulio.z@gmail.com>
2020-03-18 19:39:08 -0300

    Calculating LoC for each cloned repository (Guido's only)

--- Lab02/app.py
+++ Lab02/app.py
@@ -1,14 +1,18 @@
 from csv import writer
+from json import load
 from logging import DEBUG, INFO
-from os import getcwd, makedirs
+from os import getcwd, listdir, makedirs
 from os.path import exists
+from subprocess import Popen, PIPE
 from sys import exit
 
 import git
 import pandas as pd
 from logzero import setup_logger
+from radon import raw
 
-from GraphQL import Query
+from GitHub.GraphQL import Query
+from GitHub.tools import *
 
 l = setup_logger(name="main", level=INFO)
 
@@ -42,79 +46,9 @@ headers = {
     "Authorization": f"bearer {_token}",
 }
 
-# Some constants
-_output_path = f"{getcwd()}/output"
-
-
-def first_run(gql_query):
-    gql = gql_query
-    nodes = gql.json["data"]["search"]["nodes"]
-    table_headers = nodes[0].keys()
-    l.info(f"Total nodes after first run: {len(nodes)}")
-    return gql, table_headers, nodes
-
-
-def get_me_a_thousand(gql_query, node_list):
-    while gql_query.next_page():
-        gql_query.request()
-        l.debug(f"Total nodes after last run: {len(node_list)}")
-        node_list += gql_query.json["data"]["search"]["nodes"]
-    l.info(f"Total nodes after final run: {len(node_list)}")
-    return gql_query, node_list
-
-
-def fix_dictionaries(gql_query, node_list):
-    for i, node in enumerate(node_list):
-        node_list[i] = gql_query.fix_dict(node)
-    l.info(f"Fixed a total of {len(node_list)} node dictionaries.")
-    return gql_query, node_list
-
-
-def save_csv(file_name, table_headers, node_list):
-    if '.csv' in file_name:
-        file_name = file_name[len(file_name)-4:]
-    with open(f"{_output_path}/{file_name}.csv", "w") as f:
-        csv = writer(f)
-        csv.writerow(table_headers)
-        for repository in node_list:
-            l.debug(f"repository={repository}")
-            csv.writerow(repository.values())
-
-
 # Sprint 01
 l.info("Running Sprint 01")
 
-data_template = ("""
-{
-  search(query: "user:gvanrossum language:python", type: REPOSITORY, first: 50, after:"!<REPLACE-ME>!") {
-    repositoryCount
-    pageInfo {
-      hasNextPage
-      endCursor
-    }
-    nodes {
-      ... on Repository {
-        name
-        url
-        stargazers{totalCount}
-        watchers{totalCount}
-        forkCount
-        isFork
-        commitComments{totalCount}
-        releases{totalCount}
-        createdAt
-        primaryLanguage {
-          name
-        }
-      }
-    }
-  }
-  rateLimit {
-    remaining
-    resetAt
-  }
-}
-    """)
 
 # First run
 query_01 = Query(url, headers, data_template)
@@ -151,10 +85,38 @@ l.info("Finished Sprint 01, second step (2/4)")
 # Step 3
 df = pd.read_csv("output/guido_repos.csv")
 
+# Making temporary directory to store cloned repositories
 repos_path = f"/tmp/repositories/"
 if not exists(repos_path):
     makedirs(repos_path)
 
+# Cloning repositories
 for name, url in zip(df["name"], df["url"]):
-    repo_path = f"{repos_path}/{name}"
-    git.Git(repo_path).clone(f"{url.replace('https', 'git')}.git")
+    git.Git(repos_path).clone(f"{url.replace('https', 'git')}.git")
+    l.info(f"Cloned {name}")
+
+# Analyzing LoC in each repository
+for repo in listdir(repos_path):
+    out, err = Popen(
+        [
+            f"{getcwd()}/../venv/bin/radon",
+            "raw",
+            "-O", f"{repos_path}/{repo}.txt",
+            f"{repos_path}/{repo}"
+        ], stdout=PIPE, stderr=PIPE
+    ).communicate()
+    # Checking return code
+    if err != b"":
+        l.error(err.decode())
+    else:
+        l.debug(f"Analyzed {repo}")
+        # Summing total LoC for each repository
+        with open(f"{repos_path}/{repo}.txt", "r") as f:
+            code = f.read()
+        loc = 0
+        for line in code.splitlines():
+            if " LOC:" in line:
+                loc += int(line.split(": ")[1])
+        l.info(f"LoC for {repo}: {loc}")
+        with open(f"{repos_path}/loc_{repo}.txt", "w") as f:
+            f.write(str(loc))
